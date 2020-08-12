030d8a98c0807eb4102921e052c2644e96334c95
Ryohei Mizuho <65406188+Ryohei03@users.noreply.github.com>
2020-08-12 12:20:19 +0900

    Finish Solo MVP Project

--- src/App.jsx
+++ src/App.jsx
@@ -6,14 +6,10 @@ import Map from "./containers/Map";
 
 export default function App() {
   return (
-    <div className="App" style={{ height: "100%" }}>
+    <div className="App" style={{ height: "80%" }}>
       <div className="App-header">
-        {/* <img src={logo} className="App-logo" alt="logo" /> */}
-        <h2>Welcome to React</h2>
+        <h2>Disaster-Water-Stations-Map</h2>
       </div>
-      {/* <p className="App-intro">
-        To get started, edit <code>src/App.jsx</code> and save to reload.
-      </p> */}
       <Map id="map" />
     </div>
   );
